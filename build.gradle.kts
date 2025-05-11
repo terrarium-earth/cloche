3ab81ce808cadc1df7dd3e37a2f7c5141b5c6c1a
Ashley Wright <ashley@msrandom.net>
2025-05-11 09:44:16 +0300

    Bump codev-remapper & jvm-virtual-source-sets

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,12 +42,12 @@ dependencies {
     implementation(group = "net.msrandom", name = "minecraft-codev-mixins", version = "0.5.32")
     implementation(group = "net.msrandom", name = "minecraft-codev-runs", version = "0.5.32")
     implementation(group = "net.msrandom", name = "minecraft-codev-access-widener", version = "0.5.32")
-    implementation(group = "net.msrandom", name = "minecraft-codev-remapper", version = "0.5.32")
+    implementation(group = "net.msrandom", name = "minecraft-codev-remapper", version = "0.5.34")
     implementation(group = "net.msrandom", name = "minecraft-codev-decompiler", version = "0.5.32")
     implementation(group = "net.msrandom", name = "minecraft-codev-includes", version = "0.5.32")
 
     implementation(group = "net.msrandom", name = "class-extensions-gradle-plugin", version = "1.0.11")
-    implementation(group = "net.msrandom", name = "jvm-virtual-source-sets", version = "1.3.2")
+    implementation(group = "net.msrandom", name = "jvm-virtual-source-sets", version = "1.3.3")
     implementation(group = "net.msrandom", name = "classpath-api-stubs", version = "0.1.4")
 
     implementation(group = "com.google.devtools.ksp", name = "com.google.devtools.ksp.gradle.plugin", version = "2.1.0-1.0.29")
