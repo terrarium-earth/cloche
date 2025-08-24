c69ab5252277b0d1d8f8e2d8a88b015bccf9c0d4
Ashley Wright <ashley@msrandom.net>
2025-08-24 05:06:18 +0300

    Bump remapper & fabric modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,11 @@ java {
 dependencies {
     implementation(group = "net.msrandom", name = "minecraft-codev-core", version = "0.6.2")
     implementation(group = "net.msrandom", name = "minecraft-codev-forge", version = "0.6.3")
-    implementation(group = "net.msrandom", name = "minecraft-codev-fabric", version = "0.6.3")
+    implementation(group = "net.msrandom", name = "minecraft-codev-fabric", version = "0.6.5")
     implementation(group = "net.msrandom", name = "minecraft-codev-mixins", version = "0.5.32")
     implementation(group = "net.msrandom", name = "minecraft-codev-runs", version = "0.6.4")
     implementation(group = "net.msrandom", name = "minecraft-codev-access-widener", version = "0.5.32")
-    implementation(group = "net.msrandom", name = "minecraft-codev-remapper", version = "0.6.4")
+    implementation(group = "net.msrandom", name = "minecraft-codev-remapper", version = "0.6.5")
     implementation(group = "net.msrandom", name = "minecraft-codev-decompiler", version = "0.5.32")
     implementation(group = "net.msrandom", name = "minecraft-codev-includes", version = "0.6.0")
 
