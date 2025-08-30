103a6fe5a1b827f8bb7cdee8efec2b958567b169
Ashley Wright <ashley@msrandom.net>
2025-08-31 00:50:59 +0300

    Bump version and add comment

--- src/main/kotlin/earth/terrarium/cloche/util/ManifestFromJar.kt
+++ src/main/kotlin/earth/terrarium/cloche/util/ManifestFromJar.kt
@@ -6,6 +6,7 @@ import org.gradle.api.java.archives.Manifest
 import org.gradle.api.provider.Provider
 import java.util.jar.JarFile
 
+// TODO This should be in codev
 fun Manifest.fromJars(fileOperations: FileOperations, vararg jarFiles: Provider<RegularFile>) {
     // This is unsafe in the context of a normal provider, as you can typically query file providers without task execution, but this one returns different results before and after execution.
     //  However, in this context it is used in the merge spec for a manifest, which is only queried at execution time and thus *should* be safe
