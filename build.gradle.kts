f4f1ad7c99cb33f54f8679de2829c89cc591d64c
Ashley Wright <ashley@msrandom.net>
2026-03-26 21:25:57 -0400

    0.18.8 - attempt #2 at 0.18.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,10 +39,10 @@ java {
 dependencies {
     implementation(group = "net.msrandom", name = "minecraft-codev-core", version = "0.6.9")
     implementation(group = "net.msrandom", name = "minecraft-codev-forge", version = "0.8.1")
-    implementation(group = "net.msrandom", name = "minecraft-codev-fabric", version = "0.6.15")
+    implementation(group = "net.msrandom", name = "minecraft-codev-fabric", version = "0.7.0")
     implementation(group = "net.msrandom", name = "minecraft-codev-mixins", version = "0.6.0")
     implementation(group = "net.msrandom", name = "minecraft-codev-runs", version = "0.6.8")
-    implementation(group = "net.msrandom", name = "minecraft-codev-access-widener", version = "0.6.2")
+    implementation(group = "net.msrandom", name = "minecraft-codev-access-widener", version = "0.6.3")
     implementation(group = "net.msrandom", name = "minecraft-codev-remapper", version = "0.7.1")
     implementation(group = "net.msrandom", name = "minecraft-codev-decompiler", version = "0.6.0")
     implementation(group = "net.msrandom", name = "minecraft-codev-includes", version = "0.6.5")
