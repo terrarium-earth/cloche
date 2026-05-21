0a03ca1798e690b66f8fb122774e975d3f8f2209
SettingDust <settingdust@gmail.com>
2026-05-22 02:08:22 +0800

    chore: remove unrelated part

--- src/main/kotlin/earth/terrarium/cloche/ClocheExtension.kt
+++ src/main/kotlin/earth/terrarium/cloche/ClocheExtension.kt
@@ -2,16 +2,11 @@ package earth.terrarium.cloche
 
 import earth.terrarium.cloche.api.MappingsBuilder
 import earth.terrarium.cloche.api.attributes.CompilationAttributes
-import earth.terrarium.cloche.api.attributes.ModDistribution
 import earth.terrarium.cloche.api.attributes.IncludeTransformationStateAttribute
 import earth.terrarium.cloche.api.attributes.MinecraftModLoader
+import earth.terrarium.cloche.api.attributes.ModDistribution
 import earth.terrarium.cloche.api.metadata.RootMetadata
-import earth.terrarium.cloche.api.target.CommonTarget
-import earth.terrarium.cloche.api.target.FabricTarget
-import earth.terrarium.cloche.api.target.ForgeTarget
-import earth.terrarium.cloche.api.target.MinecraftTarget
-import earth.terrarium.cloche.api.target.NeoforgeTarget
-import earth.terrarium.cloche.api.target.targetName
+import earth.terrarium.cloche.api.target.*
 import earth.terrarium.cloche.target.common.CommonTargetInternal
 import earth.terrarium.cloche.target.fabric.FabricTargetImpl
 import earth.terrarium.cloche.target.forge.lex.ForgeTargetImpl
@@ -21,27 +16,15 @@ import net.msrandom.minecraftcodev.fabric.FabricInstallerComponentMetadataRule
 import net.msrandom.minecraftcodev.forge.RemoveNameMappingService
 import net.msrandom.minecraftcodev.includes.ExtractIncludes
 import net.msrandom.minecraftcodev.includes.StripIncludes
-import org.gradle.api.Action
-import org.gradle.api.DomainObjectCollection
-import org.gradle.api.InvalidUserCodeException
-import org.gradle.api.NamedDomainObjectContainer
-import org.gradle.api.PolymorphicDomainObjectContainer
-import org.gradle.api.Project
+import org.gradle.api.*
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.BasePluginExtension
 import org.gradle.api.provider.Property
-import org.gradle.kotlin.dsl.domainObjectContainer
-import org.gradle.kotlin.dsl.domainObjectSet
-import org.gradle.kotlin.dsl.newInstance
-import org.gradle.kotlin.dsl.polymorphicDomainObjectContainer
-import org.gradle.kotlin.dsl.property
-import org.gradle.kotlin.dsl.registerTransform
-import org.gradle.kotlin.dsl.withModule
+import org.gradle.kotlin.dsl.*
 import javax.inject.Inject
-import org.gradle.kotlin.dsl.withType
 
 internal val Project.cloche
     get() = extension<ClocheExtension>()
@@ -136,8 +119,6 @@ open class ClocheExtension @Inject constructor(private val project: Project, obj
     internal val mappingActions = project.objects.domainObjectSet(Action::class) as DomainObjectCollection<Action<MappingsBuilder>>
 
     private val singleTargetCallbacks = hashMapOf<Class<out MinecraftTarget>, (target: MinecraftTarget) -> Unit>()
-
-    internal val intermediaryMinecraftProviders = project.objects.newInstance(DefinedMinecraftProviders::class.java)
 
     private fun onTargetTypeConfigured(type: Class<out MinecraftTarget>, action: (target: MinecraftTarget) -> Unit) {
         var configured = false
